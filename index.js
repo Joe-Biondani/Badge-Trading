7d32b6c53664ea7c9a1f3251e225be584f6be3e3
Joe-Biondani <joebiondani@gmail.com>
2024-08-29 14:05:22 +0100

    Moved files to allow for nodejs & express use for loading pages. Added basic page handling and 404 page.

---
+++ index.js
@@ -0,0 +1,76 @@
+// Packages
+const express = require("express");
+const app = express();
+const slash = require("express-slash");
+const fetch = import("node-fetch");
+const path = import("path");
+const maintenance = false;
+
+// App settings
+app.enable("strict routing");
+
+const router = express.Router({
+    caseSensitive: app.get("case sensitive routing"),
+    strict: app.get("strict routing")
+});
+
+app.use(router);
+app.use(express.static(__dirname + '/public'));
+app.use(slash());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+
+// Routes
+router.get("/feed", (req, res) => {
+    res.render("feed");
+    res.status(200);
+});
+
+router.get("/", (req, res) => {
+    res.redirect("/feed")
+});
+
+router.get("/home", (req, res) => {
+    res.redirect("/feed")
+});
+
+router.get("/login", (req, res) => {
+    res.render("login");
+});
+
+router.get("/privacy", (req, res) => {
+    res.render("privacy");
+});
+
+router.get("/tos", (req, res) => {
+    res.render("tos");
+});
+
+app.use(function(req, res, next){
+    res.status(404);
+  
+    // respond with html page
+    if (req.accepts('html')) {
+      res.render('404');
+      return;
+    }
+  
+    // respond with json
+    if (req.accepts('json')) {
+      res.send({
+        status: 404,
+        error: 'Not found'
+      });
+      return;
+    }
+  
+    // default to plain-text. send()
+    res.type('txt').send('404 - Not found');
+  });
+
+// Port
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+app.listen(8000, () => console.log(`Listening on port ${8000}`));
