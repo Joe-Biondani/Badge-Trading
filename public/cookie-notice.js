18230d4463d0cffb85f54592c33b648cc1eda938
Joe-Biondani <joebiondani@gmail.com>
2024-09-12 21:12:15 +0100

    Added cookie notice to all pages to ensure users are aware of Privacy Policy.

---
+++ public/cookie-notice.js
@@ -0,0 +1,53 @@
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
+
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+waitForElm("#cookie-notice").then((cookie_notice) => {
+    if (!getCookie("ckxs_accptd")) {
+        cookie_notice.style.display = "block";
+    };
+
+    document.getElementById('cookies-accept').onclick = function(e) {
+        e.preventDefault();
+        setCookie("ckxs_accptd", "true", 999999999);
+        cookie_notice.style.display = "none";
+    };
+});
