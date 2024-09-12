18df8f7dff94aa8bc13fcef181a0f2d4bfd062fc
Joe-Biondani <joebiondani@gmail.com>
2024-09-12 21:13:08 +0100

    Now waits for elements to load.

--- public/mobile-nav.js
+++ public/mobile-nav.js
@@ -1,16 +1,36 @@
 // MOBILE NAV
+function waitForElm(selector) {
+    return new Promise(resolve => {
+        if (document.querySelector(selector)) {
+            return resolve(document.querySelector(selector));
+        }
 
-const openbtn = document.getElementById('mobile-nav-open');
-const closebtn = document.getElementById('mobile-nav-close');
-const nav = document.getElementById('mobile-nav');
-const body = document.getElementsByTagName("body")[0];
+        const observer = new MutationObserver(mutations => {
+            if (document.querySelector(selector)) {
+                observer.disconnect();
+                resolve(document.querySelector(selector));
+            }
+        });
 
-openbtn.onclick = function() {
-    nav.style = "display:block;"
-    body.style = "overflow-y:hidden;"
-};
-
-closebtn.onclick = function () {
-    nav.style = "display:none;"
-    body.style = ""
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
 }
+    
+waitForElm('#mobile-nav-open').then((openbtn) => {
+    const closebtn = document.getElementById('mobile-nav-close');
+    const nav = document.getElementById('mobile-nav');
+    const body = document.getElementsByTagName("body")[0];
+
+    openbtn.onclick = function() {
+        nav.style = "display:block;"
+        body.style = "overflow-y:hidden;"
+    };
+
+    closebtn.onclick = function () {
+        nav.style = "display:none;"
+        body.style = ""
+    }
+})
